23c0df786190e1448c20f9742dc7ce8364f1f42e
Ibrahim Kamay <ibrahimkamayweb@gmail.com>
2023-11-30 00:00:52 +0300

    updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,88 +1,133 @@
-import React, { useEffect, useState } from 'react'
-import logo from "../assets/logo.svg"
-import { Link } from 'react-scroll';
-import { FaBars, FaXmark  } from "react-icons/fa6";
-
+import React, { useEffect, useState } from "react";
+import logo from "../assets/logo.svg";
+import { Link } from "react-scroll";
+import { FaBars, FaXmark } from "react-icons/fa6";
 
 const Navbar = () => {
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
-    const [isSticky, setIsSticky] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isSticky, setIsSticky] = useState(false);
 
-    // set toggle Menu
-    const toggleMenu = () => {
-        setIsMenuOpen(!isMenuOpen);
-    }
+  // set toggle Menu
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
 
-useEffect(() => {
+  useEffect(() => {
     const handScroll = () => {
-        if (window.scrollY > 100) {
-            setIsSticky(true)
-        } else {
-            setIsSticky(false)   
-        }
+      if (window.scrollY > 100) {
+        setIsSticky(true);
+      } else {
+        setIsSticky(false);
+      }
     };
-    window.addEventListener('scroll', handScroll);
+    window.addEventListener("scroll", handScroll);
     return () => {
-    window.addEventListener('scroll', handScroll);
+      window.addEventListener("scroll", handScroll);
+    };
+  });
+  // navitems array
 
-    }
-})
-// navitems array 
-
-const NavItems = [
+  const NavItems = [
     {
-        link:"Home",
-        path: "home"
+      link: "Home",
+      path: "home",
     },
     {
-        link:"Service",
-        path: "service"
+      link: "Service",
+      path: "service",
     },
     {
-        link:"About",
-        path: "about"
+      link: "About",
+      path: "about",
     },
     {
-        link:"Product",
-        path: "product"
+      link: "Product",
+      path: "product",
     },
     {
-        link:"Testimonial",
-        path: "testimonial"
+      link: "Testimonial",
+      path: "testimonial",
     },
     {
-        link:"FAQ",
-        path: "faq"
+      link: "FAQ",
+      path: "faq",
     },
-]
+  ];
   return (
-    <header className='w-full bg-white md:bg-transparent fixed top-0 left-0 right-0'>
-        <nav>
-            <div className='flex justify-between items-center text-base gap-8'>
-                <a href=""><img src={logo} alt="logo" className='w-[128px] inline-block ' /></a>
-                {/* nav items for large devices */}
-                <ul className='md:flex space-x-12 hidden '>
-                    {
-                        NavItems.map(({link,path}) =><Link to={path} spy={true} smooth={true} offset={-100} key={path}
-                        className='block text-base text-gray900 hover:text-brandPrimary first:font-medium'>{link}</Link> )
-                    }
-                </ul>
-                <div className='space-x-12 hidden lg:flex items-center'>
-                    <a href="/" className='hidden lg:flex items-center text-brandPrimary hover:text-gray900'>Login</a>
-                    <button className='bg-brandPrimary text-white py-2 px-4 transition-all duration-300 hover:bg-neutralGrey'>Sign up</button>
-                </div>
-                {/* menu btn for only mobile devices */}
-                <div className='md:hidden'>
-                    <button className='focus:outline-none focus:text-gray-500'>
-                        {
-                            isMenuOpen ? (<FaXmark className='h-6 w-6'/>) : (<FaBars className='h-6 w-6'/>)
-                        }
-                    </button>
-                </div>
-            </div>
-        </nav>
+    <header className="w-full bg-white md:bg-transparent fixed top-0 left-0 right-0">
+      <nav
+        className={`py-4 lg:px-14 px-4 ${
+          isSticky
+            ? "sticky top-0 left-0 right-0 border-b bg-whit duration-300"
+            : ""
+        } `}
+      >
+        <div className="flex justify-between items-center text-base gap-8">
+          <a href="">
+            <img src={logo} alt="logo" className="w-[128px] inline-block " />
+          </a>
+          {/* nav items for large devices */}
+          <ul className="md:flex space-x-12 hidden ">
+            {NavItems.map(({ link, path }) => (
+              <Link
+                to={path}
+                spy={true}
+                smooth={true}
+                offset={-100}
+                key={path}
+                className="block text-base text-gray900 hover:text-brandPrimary first:font-medium"
+              >
+                {link}
+              </Link>
+            ))}
+          </ul>
+          <div className="space-x-12 hidden lg:flex items-center">
+            <a
+              href="/"
+              className="hidden lg:flex items-center text-brandPrimary hover:text-gray900"
+            >
+              Login
+            </a>
+            <button className="bg-brandPrimary text-white py-2 px-4 transition-all duration-300 hover:bg-neutralGrey">
+              Sign up
+            </button>
+          </div>
+          {/* menu btn for only mobile devices */}
+          <div className="md:hidden">
+            <button
+              onClick={toggleMenu}
+              className="focus:outline-none focus:text-gray-500"
+            >
+              {isMenuOpen ? (
+                <FaXmark className="h-6 w-6" />
+              ) : (
+                <FaBars className="h-6 w-6" />
+              )}
+            </button>
+          </div>
+        </div>
+        {/*  nav items for mobile devices */}
+        <div
+          className={`space-y-4 px-4 mt-16 py-7 bg-brandPrimary ${
+            isMenuOpen ? "block fixed top-0 right-0 left-0 " : "hidden"
+          } `}
+        >
+          {NavItems.map(({ link, path }) => (
+            <Link
+              to={path}
+              spy={true}
+              smooth={true}
+              offset={-100}
+              key={path}
+              className="block text-base text-gray900 hover:text-brandPrimary first:font-medium"
+            >
+              {link}
+            </Link>
+          ))}
+        </div>
+      </nav>
     </header>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
-        'neutralSilver' : '#F5F7FA',
-        'neutralDGrey' : '#4D4D4D',
-        'brandPrimary' : '#4CAF4F',
-        'neutralGrey' : '#717171',
-        'gray900' : '#18191F'
+        neutralSilver: "#F5F7FA",
+        neutralDGrey: "#4D4D4D",
+        brandPrimary: "#4CAF4F",
+        neutralGrey: "#717171",
+        gray900: "#18191F",
       },
     },
   },
   plugins: [],
-}
+};
