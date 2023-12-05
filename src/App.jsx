96b8ffc09449232c62744257f2f38059c1bc1612
Ibrahim Kamay <ibrahimkamayweb@gmail.com>
2023-12-05 23:21:05 +0300

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import './App.css'
-import Navbar from './components/Navbar'
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
 
 function App() {
-  
-
   return (
     <>
-   <Navbar/>
+      <Navbar />
+      <Home />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
